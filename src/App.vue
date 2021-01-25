4d994df7b702cfa26bd1f513ab3f5cb845475c96
jurvantoledo <jurvantoledo@live.nl>
2021-01-25 14:25:16 +0100

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,69 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Vue 3 TODO APP</h1>
+  <form @submit.prevent="addNewTodo">
+    <label>New Todo</label>
+    <input v-model="newTodo" name="newTodo" />
+    <button>Add new Todo</button>
+  </form>
+  <button @click="removeAllTodos">Remove All</button>
+  <button @click="markAllDone">Mark all done</button>
+  <ul>
+    <li v-for="(todo, index) in todos" :key="todo.id" class="todo">
+      <h3 :class="{ done: todo.done }" @click="toggleDone(todo)">
+        {{ todo.content }}
+      </h3>
+      <button @click="removeTodo(index)">Remove todo</button>
+    </li>
+  </ul>
 </template>
 
+// :class="{ done: todo.done }" says if todo.done === true apply styling to
+class done
+
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  setup() {
+    const newTodo = ref("");
+    const todos = ref([]); //created an array of todos
+
+    function addNewTodo() {
+      todos.value.push({
+        id: Date.now(), //normally dont use Date.now for id !
+        done: false,
+        content: newTodo.value,
+      }); // When function is called pushed object in the todos array !
+      newTodo.value = ""; // So the input field is empty after button is clicked !
+    }
+
+    function toggleDone(todo) {
+      todo.done = !todo.done;
+    }
+
+    function removeTodo(index) {
+      todos.value.splice(index, 1);
+    } // This function will now remove 1 specific element of that index
+
+    function markAllDone() {
+      todos.value.forEach((todo) => (todo.done = true));
+    }
+
+    function removeAllTodos() {
+      todos.value = []; //Removes all todos by setting array to empty
+    }
+
+    return {
+      newTodo,
+      addNewTodo,
+      todos,
+      toggleDone,
+      removeTodo,
+      markAllDone,
+      removeAllTodos,
+    };
+  },
+};
 </script>
 
 <style>
@@ -22,5 +74,13 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.todo {
+  cursor: pointer;
+}
+
+.done {
+  text-decoration: line-through;
 }
 </style>
